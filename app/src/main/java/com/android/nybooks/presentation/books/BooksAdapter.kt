81423ee5b1329b6df5fe5e160230cfd7e7503025
Kaleb <kaleb.silva@itbam.org.br>
2019-10-28 16:48:55 -0400

    adicionado a biblioteca data binding para listagem de books

--- app/src/main/java/com/android/nybooks/presentation/books/BooksAdapter.kt
+++ app/src/main/java/com/android/nybooks/presentation/books/BooksAdapter.kt
@@ -1,39 +1,39 @@
 package com.android.nybooks.presentation.books
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.android.nybooks.R
 import com.android.nybooks.data.model.Book
-import kotlinx.android.synthetic.main.item_book.view.*
+import com.android.nybooks.databinding.ItemBookBinding
+
 
 class BooksAdapter(
         val books: List<Book>,
         val onItemClickListener: ((book:Book) -> Unit) // calback
 ) : RecyclerView.Adapter<BooksAdapter.BooksViewHolder>(){
 
+    //criacao de cada item do recyclerView
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BooksViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_book, parent, false)
-        return  BooksViewHolder(view, onItemClickListener)
+        //view = item de layout
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemBookBinding.inflate(inflater)
+
+        return  BooksViewHolder(binding, onItemClickListener)
     }
 
     override fun getItemCount() = books.count()
 
+    //popula os item com dados
     override fun onBindViewHolder(holder: BooksViewHolder, position: Int) {
                 holder.bindView(books[position])
     }
 
-    class BooksViewHolder(itemView: View,
+    class BooksViewHolder(val binding: ItemBookBinding,
                           private val onItemClickListener: ((book:Book) -> Unit))
-        : RecyclerView.ViewHolder(itemView){
-        private val title = itemView.textTitle
-        private val author = itemView.textAuthor
+        : RecyclerView.ViewHolder(binding.root){
 
             fun bindView(book: Book){
-                title.text = book.title
-                author.text = book.author
-
+               binding.book = book
                 itemView.setOnClickListener {
                     onItemClickListener.invoke(book)
                 }
